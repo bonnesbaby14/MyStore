7b2b1fcbbbf5935e76889296c6a2ab6147ec298c
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-09-04 23:43:25 -0500

    implementacion para los gradiant

--- android/app/src/main/java/com/mystore/MainApplication.java
+++ android/app/src/main/java/com/mystore/MainApplication.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Arrays;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
 import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import com.BV.LinearGradient.LinearGradientPackage; 
 public class MainApplication extends Application implements ReactApplication {
  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
   private final ReactNativeHost mReactNativeHost =
@@ -31,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new MyReactNativePackage());
            // Add unimodules
           List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
-            new ModuleRegistryAdapter(mModuleRegistryProvider)
+            new ModuleRegistryAdapter(mModuleRegistryProvider),
+            new LinearGradientPackage()
           );
           packages.addAll(unimodules);
           return packages;
