c64e2585628dbd448e09b1450a5fb8bcad19653a
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-09-06 21:26:34 -0500

    inicio de colores en

---
+++ pages/Records.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import NavigationBar from 'react-native-navbar-color';
+import {Icon} from 'react-native-elements';
+import Item from '../components/Item';
+
+const Records =()=>{
+    const colorclaro = '#20045e';
+    const colorFondo = '#1A1F2B';
+    const colorBTN = '#1f2844';
+    const colorInput = '#535865';
+  
+    const colorStatus = '#7226dd';
+    NavigationBar.setColor('#29064e');
+
+
+    return ();
+}
+
+export default Records;
