8972209225f3e1399f7c3132b15851526a9a88d7
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-09-19 19:09:05 -0500

    se añadio el componente del scanner

--- components/Scanner.js
+++ components/Scanner.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,11 +11,33 @@ import {
   TouchableOpacity,
   ScrollView,
   Modal,
+  Alert,
   Pressable,
 } from 'react-native';
 import {FloatingLabelInput} from 'react-native-floating-label-input';
+import {BarCodeScanner} from 'expo-barcode-scanner';
 
 const Scanner = ({estado, setEstado}) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({type, data}) => {
+    setScanned(true);
+    Alert.alert(
+      'Alert Title',
+      `Bar code with type ${type} and data ${data} has been scanned!`, // <- this part is optional, you can pass an empty string
+      [{text: 'OK', onPress: () => setScanned(false)}],
+      {cancelable: false},
+    );
+  };
+
   const styles = StyleSheet.create({
     containerModal: {
       flex: 1,
@@ -29,12 +51,51 @@ const Scanner = ({estado, setEstado}) => {
 
     content: {
       flexDirection: 'column',
-      height: 400,
+      height: 600,
       width: '85%',
       backgroundColor: 'rgba(36,22,50,0.8)',
       alignItems: 'center',
       justifyContent: 'center',
       borderRadius: 10,
+    },
+    camera: {
+      backgroundColor: 'gold',
+      flex: 4,
+      width: '100%',
+    },
+
+    preview: {
+      backgroundColor: 'green',
+      flex: 2,
+      width: '100%',
+    },
+
+    botones: {
+      backgroundColor: 'blue',
+      flex: 1,
+      width: '100%',
+      flexDirection: 'row',
+    },
+
+    add: {
+      flex: 1,
+      backgroundColor: 'purple',
+    },
+
+    accept: {flex: 1, backgroundColor: 'orange'},
+    botonesModal: {
+      backgroundColor: 'transparent',
+      borderRadius: 5,
+      margin: 10,
+      flex: 1,
+      width: '90%',
+      height: 45,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      borderColor: '#FFF',
+      borderWidth: 1,
+      color: '#FFF',
     },
   });
 
@@ -47,7 +108,31 @@ const Scanner = ({estado, setEstado}) => {
         setEstado(false);
       }}>
       <View style={styles.containerModal}>
-        <View style={styles.content}></View>
+        <View style={styles.content}>
+          <View style={styles.camera}>
+            {hasPermission === null ? (
+              <Text>Requesting for camera permission</Text>
+            ) : hasPermission === false ? (
+              <Text>No access to camera</Text>
+            ) : (
+              <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={{flex: 1, width: '100%'}}
+              />
+            )}
+          </View>
+          <View style={styles.preview}></View>
+          <View style={styles.botones}>
+            <View style={styles.add}></View>
+            <View style={styles.accept}>
+              <TouchableOpacity
+                style={styles.botonesModal}
+                onPress={() => setEstado(false)}>
+                <Text style={{color: '#FFF'}}>Aceptar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
     </Modal>
   );
